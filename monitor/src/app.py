c2012fcca75b9cc5233cf67983c0806479eea157
40324582 <tbrownlee02@qub.ac.uk>
2024-12-11 04:06:33 +0000

    adding updates to the service

--- monitor/src/app.py
+++ monitor/src/app.py
@@ -5,28 +5,33 @@ from monitor.src.email_service import service_down_email
 import requests
 import time
 
-ping_list_of_services = discovery.get_active_services()
+existing_service_list = discovery.get_active_services()
 
 
 
 def ping_services():
     exclude_services = ["front-end","char-count", "word-count", "monitor"]
     while True:
-        time.sleep(3)
-        ping_list_of_services = discovery.get_active_services()
+        time.sleep(15)
+        print("Pinging Services in list")
+        latest_service_list = discovery.get_active_services()
 
-        for service in ping_list_of_services['active_services']:
+        for service in latest_service_list['active_services']:
             if service in exclude_services:
                 continue
-            ip_addresses = ping_list_of_services['active_services'][service].keys()
+            ip_addresses = latest_service_list['active_services'][service].keys()
             for address in ip_addresses:
-                url = f"{address}/api/{service}/service/ping"
-                response = requests.get(url, timeout=10)
-                if response.status_code != 200:
+                try:
+                    url = f"{address}/api/{service}/service/ping"
+                    response = requests.get(url, timeout=10)
+                    if response.status_code != 200:
+                        service_down_email.send_service_down(service)
+                    json_data = response.json()
+                    if (json_data["status"] != "active"):
+                        service_down_email.send_service_down(service)
+                except requests.exceptions.RequestException:
                     service_down_email.send_service_down(service)
-                json_data = response.json()
-                if (json_data["status"] != "active"):
-                    service_down_email.send_service_down(service)
+        existing_service_list = latest_service_list
 
     
         
