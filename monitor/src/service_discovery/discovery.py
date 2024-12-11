308fac53805afcc6485b2bfa9fb03e647ce210e8
40324582 <tbrownlee02@qub.ac.uk>
2024-12-11 00:12:39 +0000

    adding fixes to the linting

--- monitor/src/service_discovery/discovery.py
+++ monitor/src/service_discovery/discovery.py
@@ -1,17 +1,22 @@
 """Calls on the docker compose api to get the information about other container in the image"""
-import subprocess
+
 import json
+import subprocess
+
 
 def get_docker_containers_in_network():
     """Retrieves a list of Docker containers within a specific network using Docker CLI."""
     command = [
-        "curl", "--unix-socket", "/var/run/docker.sock", 
-        "http://localhost/containers/json"
+        "curl",
+        "--unix-socket",
+        "/var/run/docker.sock",
+        "http://localhost/containers/json",
     ]
     result = subprocess.run(command, capture_output=True, text=True, check=True)
 
     containers = json.loads(result.stdout)
     return containers
+
 
 if __name__ == "__main__":
     containers = get_docker_containers_in_network()
